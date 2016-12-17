032de644496679a96e79453d1ec7cefb2b8ce178
Antonio <antoniojgage@gmail.com>
2016-12-16 18:15:21 -0600

    Changed HTML, JS & CSS to be seperate. Mapped all functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-
     var map;
     var infowindow;
     //need to ask user for this
@@ -7,39 +5,48 @@ $(document).ready(function() {
     var interest = "pizza";
     var keyword;
     //plug user location in here
-    var lat;
-    var lng;
-    var user = { lat: lat, lng: lng };
+    // var latitude = 30.4704588;
+    // var longitude = -97.68593229999999;
 
-    var queryURL = "https://api.yelp.com/v2/search?term=" + interest + "&location=" + user;
+    lat = 30.4704588;
+    lng = -97.68593229999999;
+    var user = {
+        lat: lat,
+        lng: lng
+    };
 
-    $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
-        console.log(response);
-    });
+    // var queryURL = "https://api.yelp.com/v2/search?term=" + interest + "&location=" + user;
+    // console.log(queryURL);
+    // $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
+    //     console.log(response);
+    // });
 
     function initMap() {
-
+        console.log("Latitude here in Map : " + user.lat)
         map = new google.maps.Map(document.getElementById('map'), {
             center: user,
             //need to figure appropriate zoom (maybe based on how far of a radius the user chooses)
             zoom: 11
         });
 
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
+
+        var request = {
             location: user,
-            //once again need to determine the radius based on user input
-            radius: 50000,
-            //take the top names from yelp results and put them in here (how to do multiple keywords on one map?)
-            keyword: 'top golf'
-        }, callback);
+            radius: '500',
+            query: interest
+        };
+
+        infowindow = new google.maps.InfoWindow();
+        console.log(map);
+        var service = new google.maps.places.PlacesService(map);
+
+        service.nearbySearch(request, callback);
+        console.log("service finished bro")
     }
 
     function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
-
                 createMarker(results[i]);
             }
         }
@@ -71,12 +78,10 @@ $(document).ready(function() {
             var latitude = position.coords.latitude;
             var longitude = position.coords.longitude;
 
-            output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-            var img = new Image();
-            img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-            output.appendChild(img);
+            output.innerHTML = '<p>Latitude is ' + latitude + '&deg; <br>Longitude is ' + longitude + '&deg;</p>';
+            // var img = new Image();
+            // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+            // output.appendChild(img);
         }
 
         function error() {
@@ -87,12 +92,88 @@ $(document).ready(function() {
 
         navigator.geolocation.getCurrentPosition(success, error);
     }
-    // function(d, s, id) {
-    //     var js, fjs = d.getElementsByTagName(s)[0];
-    //     if (d.getElementById(id)) return;
-    //     js = d.createElement(s);
-    //     js.id = id;
-    //     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
-    //     fjs.parentNode.insertBefore(js, fjs);
-    // }(document, 'script', 'facebook-jssdk');
-});
+
+    // This is called with the results from from FB.getLoginStatus().
+    function statusChangeCallback(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+        // The response object is returned with a status field that lets the
+        // app know the current login status of the person.
+        // Full docs on the response object can be found in the documentation
+        // for FB.getLoginStatus().
+        if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            testAPI();
+        } else if (response.status === 'not_authorized') {
+            // The person is logged into Facebook, but not your app.
+            document.getElementById('status').innerHTML = 'Please log ' +
+                'into this app.';
+        } else {
+            // The person is not logged into Facebook, so we're not sure if
+            // they are logged into this app or not.
+            document.getElementById('status').innerHTML = 'Please log ' +
+                'into Facebook.';
+        }
+    }
+
+    // This function is called when someone finishes with the Login
+    // Button.  See the onlogin handler attached to it in the sample
+    // code below.
+    function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+    }
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '858186090985499',
+            cookie: true, // enable cookies to allow the server to access 
+            // the session
+            xfbml: true, // parse social plugins on this page
+            version: 'v2.8' // use graph api version 2.8
+        });
+
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+
+    };
+
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    // Here we run a very simple test of the Graph API after login is
+    // successful.  See statusChangeCallback() for when this call is made.
+    function testAPI() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+            console.log('Successful login for: ' + response.name);
+            document.getElementById('status').innerHTML =
+                'Thanks for logging in, ' + response.name + '!';
+        });
+    }
+
+
+    function myFacebookLogin() {
+        FB.login(function() {}, {
+            scope: 'publish_actions'
+        });
+    }
+
+    $("#myFacebookLogin").on("click", function(event) {
+        myFacebookLogin();
+    });
+
+    $("#geoFindMe").on("click", function(event) {
+        geoFindMe();
+        console.log("Calling Functions");
+        // initMap();
+    });
