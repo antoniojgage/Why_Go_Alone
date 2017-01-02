731b6fddd404ef096bb12292c716393cf4654901
Mary Willis <msibilsk@gmail.com>
2017-01-02 11:12:14 -0600

    trying again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
             console.log(uid);
             usersDatabase.ref("/users").on("child_added", function(snap) {
                 console.log("checking uid");
-                console.log(snap.val().child().uid);
+                console.log(snap.val().uid);
                 // if (snap.child("uid").val() === uid) {
                 //     console.log(snap.child("uid").val());
                 //     alert("Josh already exists");
